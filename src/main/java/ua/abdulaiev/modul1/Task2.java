413ec4d83de4e527c1027591d5bc59b38ae4858b
Ibragim-abdulaev <iabdulaeff6@gmail.com>
2021-09-07 12:18:20 +0300

    Update file Task2 in module1

--- src/main/java/ua/abdulaiev/modul1/Task2.java
+++ src/main/java/ua/abdulaiev/modul1/Task2.java
@@ -7,27 +7,33 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner inputUser = new Scanner(System.in);
         System.out.println("Ход коня в формате 'b1-c3' или 'x' для выхода");
-        String  currPos;
-        String  nextPos;
         String muve = inputUser.next();
+
         while (!"x".equals(muve)) {
-            currPos = muve.split("-")[0].toLowerCase();
-            nextPos = muve.split("-")[1].toLowerCase();
-            if (isRightMuve(currPos.charAt(0), Character.digit(currPos.charAt(1),10),
-                    nextPos.charAt(0), Character.digit(nextPos.charAt(1),10))) {
-                System.out.println("Ход верный");
-            } else System.out.println("Так конь не ходит!");
-            System.out.println("?");
+            run(muve);
             muve = inputUser.next();
         }
     }
-    public static boolean isRightPositionOnBoard(char letter, int num) {
-        return (letter < 'a' || letter > 'h') || (num < 1 || num > 8);
+
+    static void run(String muve) {
+        String currPos = muve.split("-")[0].toLowerCase();
+        String nextPos = muve.split("-")[1].toLowerCase();
+        if (isRightMuve(currPos.charAt(0), Character.digit(currPos.charAt(1), 10),
+                nextPos.charAt(0), Character.digit(nextPos.charAt(1), 10))) {
+            System.out.println("Ход верный");
+        } else {
+            System.out.println("Так конь не ходит!");
+        }
+        System.out.println("?");
     }
 
-    public static boolean isRightMuve(char letCurrPos, int numCurrPos, char letNewPos, int numNewPos) {
-        return (!isRightPositionOnBoard(letCurrPos, numCurrPos) ||
-                !isRightPositionOnBoard(letNewPos, numNewPos)) &&
+    static boolean isRightPositionOnBoard(char letter, int num) {
+        return ((letter >= 'a' && letter <= 'h') && (num >= 1 && num <= 8));
+    }
+
+    static boolean isRightMuve(char letCurrPos, int numCurrPos, char letNewPos, int numNewPos) {
+        return (isRightPositionOnBoard(letCurrPos, numCurrPos) &&
+                isRightPositionOnBoard(letNewPos, numNewPos)) &&
                 Math.abs(letCurrPos - letNewPos) + Math.abs(numCurrPos - numNewPos) == 3 &&
                 Math.abs(letCurrPos - letNewPos) < 3 && Math.abs(numCurrPos - numNewPos) < 3;
     }
