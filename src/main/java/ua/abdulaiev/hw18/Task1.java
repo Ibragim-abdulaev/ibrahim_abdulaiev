b7a8610295a0cd38e7c8e2049e299a71e600ce4c
Ibragim-abdulaev <iabdulaeff6@gmail.com>
2021-10-06 18:28:35 +0300

    Updated file in folder hw18

--- src/main/java/ua/abdulaiev/hw18/Task1.java
+++ src/main/java/ua/abdulaiev/hw18/Task1.java
@@ -1,9 +1,7 @@
 package ua.abdulaiev.hw18;
 
-import java.io.IOException;
-
 public class Task1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Box boxJson = new Box();
         ServiceBoxJson.fillFromJSON(boxJson);
         System.out.println(boxJson + System.lineSeparator());
